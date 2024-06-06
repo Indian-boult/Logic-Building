4dc5e6bf44f9c58a0673ebc840e41876a5126a91
Indian-boult <64060864+Indian-boult@users.noreply.github.com>
2024-06-06 20:03:06 +0530

    Time: 4 ms (99.74%), Space: 45.2 MB (52.56%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean palin(int left,int right,String s){
+        while(left<right){
+            if(s.charAt(left)!=s.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    public boolean validPalindrome(String s) {
+        boolean once =false;
+        int left=0;
+        int right = s.length()-1;
+        while(left<right){
+            if(s.charAt(left)!=s.charAt(right)){
+                return palin(left+1,right,s) || palin(left,right-1,s);
+            }
+            left++;
+            right--;
+        }
+        return true;
+        
+    }
+}
